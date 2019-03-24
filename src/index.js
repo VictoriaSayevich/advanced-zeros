3a43beee82e89981d82fda78f379fb4652677703
Victoria Sayevich <VictoriaSayevich@gmail.com>
2019-03-24 12:57:40 +0200

    feat: task4 advanced-zeros completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+function factorial(n) {
+  var array = [];
+  for (var f = 2, p = 0; f <= n; f++, p = 0) {
+    while (n % f === 0) {
+      n /= f;
+      p++;
+    }
+    if (p) {
+      array.push( { f, p } );
+    }
+  }
+  return array;
+}
+
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const factors = factorial(base);
+  var count = 0;
+
+  factors.forEach( f => {
+      var result_f = 0, base_p = f.f, d = base_p;
+      while ( Math.floor(number / d) >= 1 ) {
+        result_f += Math.floor(number / d);
+        d *= base_p;
+      }
+      result_f /= f.p;
+      if (!count || result_f < count) count = result_f;
+    }
+  );
+  return Math.floor(count);
 }
